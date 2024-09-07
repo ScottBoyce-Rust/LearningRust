f11cf300dd0478ba7ee2bf088c0c43c69cd3ce43
Scott Boyce <seboyce@usgs.gov>
2024-09-07 13:46:55 -0700

    rusty-journal from microsoft setup main with .expect

--- src/microsoft-learn/rusty-journal/src/main.rs
+++ src/microsoft-learn/rusty-journal/src/main.rs
@@ -1,7 +1,26 @@
 mod cli;
 mod tasks;
+
 use structopt::StructOpt;
 
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    // Unpack the journal file.
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    // Perform the action.
+    match action {
+        Add { task } => tasks::add_task(journal_file, Task::new(task)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Failed to perform action")
 }
