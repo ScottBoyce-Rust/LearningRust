242b0474febc8cf6300457821c0ebc368af10195
Scott Boyce <seboyce@usgs.gov>
2024-09-07 16:34:43 -0700

    rusty-journal from microsoft finalize with anyhow error msg

--- src/microsoft-learn/rusty-journal/src/main.rs
+++ src/microsoft-learn/rusty-journal/src/main.rs
@@ -4,6 +4,7 @@ mod tasks;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
 
+use anyhow::anyhow;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -14,7 +15,7 @@ fn find_default_journal_file() -> Option<PathBuf> {
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // Get the command-line arguments.
     let CommandLineArgs {
         action,
@@ -24,13 +25,13 @@ fn main() {
     // Unpack the journal file.
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file.");
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     // Perform the action.
     match action {
         Add { task } => tasks::add_task(journal_file, Task::new(task)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+    Ok(())
 }
